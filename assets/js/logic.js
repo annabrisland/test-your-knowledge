4915296fc68be6207f8e47f51def9fc015ac749b
annabrisland <84166964+annabrisland@users.noreply.github.com>
2023-11-03 16:06:51 -0700

    Displays first question on start

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,32 @@
 var start = document.querySelector("#start");
 var time = document.querySelector("#time");
+var startEl = document.querySelector("#start-screen");
+var questionEl = document.querySelector("#questions");
+var questionName = document.querySelector("#question-title");
+var questionChoices = document.querySelector("#choices");
 
+// Declare function to display questions
+function displayQ() {
+    // Hide start screen
+    startEl.className = "hide";
+    // Display first question
+    questionEl.className = "";
+    // loop through questions array and display the question
+    questionName.textContent = questions[0].question;
+    // Create each question choice
+    for (var j = 0; j < questions[0].answers.length; j++) {
+            var button = document.createElement("button");
+            
+            button.textContent = questions[0].answers[j];
+            // Allocate each choice an index
+            button.dataset.index = j;
+            questionChoices.appendChild(button);
+    }
+}
+
+// Declare countdown function
 function countdown() {
+    // Set full time
     var timeRemaining = 30;
 
     var timeInterval = setInterval(function() {
@@ -11,12 +36,15 @@ function countdown() {
         if (timeRemaining === 0) {
             clearInterval(timeInterval);
             // display time's up page HERE
-            time.textContent = ""
+            time.textContent = "";
         };
     }, 1000);
 }
 
+// 
 start.addEventListener("click", function(event) {
-    console.log("yo");
+    // Begin countdown
     countdown();
+    // Display questions
+    displayQ();
 });
