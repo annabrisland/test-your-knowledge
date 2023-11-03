dac0f32323429d6314e270f58cf3e5aa46409132
annabrisland <84166964+annabrisland@users.noreply.github.com>
2023-11-03 14:37:33 -0700

    Adds initial questions

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+var questions = [
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        answers: ["quotes", "curly brackets", "parenthesis"],
+        answerIndex: 0
+    },
+    {
+        question: "Which is NOT a commonly used datatype?",
+        answers: ["boolean", "number", "alert", "string"],
+        answerIndex: 2
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ______.",
+        answers: ["quotes", "curly brackets", "parenthesis"],
+        answerIndex: 2
+    }
+]
