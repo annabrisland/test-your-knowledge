656a9f5b9dde8f8a1c369cb1e2de4d20ce330d17
annabrisland <84166964+annabrisland@users.noreply.github.com>
2023-11-04 13:28:15 -0700

    Adds score tracking

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,53 +5,66 @@ var questionEl = document.querySelector("#questions");
 var questionName = document.querySelector("#question-title");
 var questionChoices = document.querySelector("#choices");
 var end = document.querySelector("#end-screen");
-// var answered = false;
 var questionIndex = 0;
+var score = 0;
 
-// Function to listen for answer and display response
-function getA(questionIndex) {
-    // Receive answer
-    questionEl.addEventListener("click", function(event) {
-        var element = event.target;
-        // answered = true;
+// Function to correct and score
+function correctAns(event) {
+  var element = event.target;
 
-        // Display result (correct or incorrect)
-        if (element.matches("button") && element.dataset.index == questions[questionIndex].answerIndex) {
-            var response = "Correct!";
-        } else if (element.matches("button")) {
-            var response = "Incorrect!";
-        };
+  // Display result (correct or incorrect)
+  if (
+    element.matches("button") &&
+    element.dataset.index == questions[questionIndex].answerIndex
+  ) {
+    var response = "Correct!";
+    score++;
+    console.log("correct");
+  } else if (element.matches("button")) {
+    var response = "Incorrect!";
+    console.log("incorrect");
+    score--;
+  }
 
-        var result = document.createElement("p");
-        result.textContent = response;
-        questionEl.appendChild(result);
+  var result = document.createElement("p");
+  result.textContent = response;
+  questionEl.appendChild(result);
 
-        // Append time
-        // Append score
-        // Get next question
-        getQ()
+  questionIndex++;
 
-    })
+  // Append time
+  // Append score
+  // Get next question unless no more questions
+  if(questionIndex === questions.length) {
+    return;
+  };
+  
+  getQ();
+  console.log(score);
+}
+
+// Function to listen for answer
+function getA() {
+  // Receive answer
+  questionEl.addEventListener("click", correctAns);
 }
 
 // Function to retrieve and display question
 function getQ() {
-    questionName.textContent = questions[questionIndex].question;
-    questionChoices.innerHTML = "";
+  questionName.textContent = questions[questionIndex].question;
+  questionChoices.innerHTML = "";
 
-    // Create each question choice
-    for (var j = 0; j < questions[questionIndex].answers.length; j++) {
-        var button = document.createElement("button");
+  // Create each question choice
+  for (var j = 0; j < questions[questionIndex].answers.length; j++) {
+    var button = document.createElement("button");
 
-        button.textContent = questions[questionIndex].answers[j];
-        // Allocate each choice an index
-        button.dataset.index = j;
-        questionChoices.appendChild(button);
-    }
-    // Get answers to each question
-    getA(questionIndex);
-
-    questionIndex++;
+    button.textContent = questions[questionIndex].answers[j];
+    // Allocate each choice an index
+    button.dataset.index = j;
+    questionChoices.appendChild(button);
+  }
+  // Get answers to each question
+  getA();
 }
 
 // Declare function to display questions
@@ -60,10 +73,9 @@ function displayQ() {
   startEl.className = "hide";
   // Display questions element
   questionEl.className = "";
-  
+
   // get question
   getQ();
-  
 }
 
 // Declare countdown function
@@ -75,7 +87,7 @@ function countdown() {
     timeRemaining--;
     time.textContent = timeRemaining + " Seconds";
 
-    if (timeRemaining === 0) {
+    if (timeRemaining === 0 | questionIndex === questions.length) {
       clearInterval(timeInterval);
       // Hide questions and display end screen
       questionEl.className = "hide";
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -13,5 +13,15 @@ var questions = [
         question: "The condition in an if/else statement is enclosed within ______.",
         answers: ["quotes", "curly brackets", "parenthesis"],
         answerIndex: 2
+    },
+    {
+        question: "Which is NOT a commonly used datatype?",
+        answers: ["boolean", "number", "alert", "string"],
+        answerIndex: 2
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ______.",
+        answers: ["quotes", "curly brackets", "parenthesis"],
+        answerIndex: 2
     }
 ]
