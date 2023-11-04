57a75922fad91400c14dd592873ad2bc8042e9cc
annabrisland <84166964+annabrisland@users.noreply.github.com>
2023-11-03 17:29:02 -0700

    Displays answer response

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,50 +5,87 @@ var questionEl = document.querySelector("#questions");
 var questionName = document.querySelector("#question-title");
 var questionChoices = document.querySelector("#choices");
 var end = document.querySelector("#end-screen");
+var answered = false;
 
-// Declare function to display questions
-function displayQ() {
-    // Hide start screen
-    startEl.className = "hide";
-    // Display first question
-    questionEl.className = "";
-    // loop through questions array and display the question
-    questionName.textContent = questions[0].question;
+
+// Function to listen for answer and display response
+function getA(i) {
+    // Receive answer
+    questionEl.addEventListener("click", function(event) {
+        var element = event.target;
+        answered = true;
+
+        // Display result (correct or incorrect)
+        if (element.matches("button") && element.dataset.index == questions[i].answerIndex) {
+            var response = "Correct!";
+        } else if (element.matches("button")) {
+            var response = "Incorrect!";
+        };
+
+        var result = document.createElement("p");
+        result.textContent = response;
+        questionEl.appendChild(result);
+        // Append time
+        // Append score
+
+    })
+}
+
+// Function to retrieve and display question
+function getQ(i) {
+    questionName.textContent = questions[i].question;
+    questionChoices.innerHTML = "";
+
     // Create each question choice
-    for (var j = 0; j < questions[0].answers.length; j++) {
+    for (var j = 0; j < questions[i].answers.length; j++) {
         var button = document.createElement("button");
-            
-        button.textContent = questions[0].answers[j];
+
+        button.textContent = questions[i].answers[j];
         // Allocate each choice an index
         button.dataset.index = j;
         questionChoices.appendChild(button);
     }
+
+    getA(i);
+}
+
+// Declare function to display questions
+function displayQ() {
+  // Hide start screen
+  startEl.className = "hide";
+  // Display first question
+  questionEl.className = "";
+  // loop through questions array and display the question
+  for (var i = 0; i < questions.length; i++) {
+    // get question
+    getQ(i);
+  }
 }
 
 // Declare countdown function
 function countdown() {
-    // Set full time
-    var timeRemaining = 30;
+  // Set full time
+  var timeRemaining = 30;
 
-    var timeInterval = setInterval(function() {
-        timeRemaining--;
-        time.textContent = timeRemaining + " Seconds";
+  var timeInterval = setInterval(function () {
+    timeRemaining--;
+    time.textContent = timeRemaining + " Seconds";
 
-        if (timeRemaining === 0) {
-            clearInterval(timeInterval);
-            // Hide questions and display end screen
-            questionEl.className = "hide";
-            end.className = "";
-            // Clear timer text
-            time.textContent = "";
-        };
-    }, 1000);
+    if (timeRemaining === 0) {
+      clearInterval(timeInterval);
+      // Hide questions and display end screen
+      questionEl.className = "hide";
+      end.className = "";
+      // Clear timer text
+      time.textContent = "";
+    }
+  }, 1000);
 }
 
-// 
-start.addEventListener("click", function(event) {
-    // Begin countdown
-    countdown();
-    // Display questions
-    displayQ();
+//
+start.addEventListener("click", function (event) {
+  // Begin countdown
+  countdown();
+  // Display questions
+  displayQ();
 });
