a6cce5ffa1bb537011b1273026afd9dfd1e15173
annabrisland <84166964+annabrisland@users.noreply.github.com>
2023-11-04 18:18:18 -0700

    Adds highscore table clear functionality

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,10 +1,19 @@
 var table = document.querySelector("#highscores-table");
+var clear = document.querySelector("#clear");
 var currentHighscores = JSON.parse(localStorage.getItem("highscores"));
 
 for (var i = 0; i < currentHighscores.length; i++) {
-    console.log(currentHighscores[i].name)
-    var li = document.createElement("li");
-    li.textContent = currentHighscores[i].name + ": " + currentHighscores[i].score;
-    table.appendChild(li);
-};
+  console.log(currentHighscores[i].name);
+  var li = document.createElement("li");
+  li.textContent =
+    currentHighscores[i].name + ": " + currentHighscores[i].score;
+  table.appendChild(li);
+}
 
+// Clear hichscores on click of 'clear' button
+clear.addEventListener("click", function () {
+  // Remove rendered highscores
+  table.innerHTML = "";
+  // Empty highscores local storage
+  localStorage.setItem("highscores", "");
+});
