40e25073c36e6f94fc15ba71566bff21a3a32986
annabrisland <84166964+annabrisland@users.noreply.github.com>
2023-11-04 20:00:05 -0700

    Adds new questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,7 +1,7 @@
 var questions = [
     {
         question: "String values must be enclosed within _______ when being assigned to variables.",
-        answers: ["quotes", "curly brackets", "parenthesis"],
+        answers: ["quotes", "curly brackets", "parentheses"],
         answerIndex: 0
     },
     {
@@ -11,17 +11,27 @@ var questions = [
     },
     {
         question: "The condition in an if/else statement is enclosed within ______.",
-        answers: ["quotes", "curly brackets", "parenthesis"],
+        answers: ["quotes", "curly brackets", "parentheses"],
         answerIndex: 2
     },
     {
-        question: "Which is NOT a commonly used datatype?",
-        answers: ["boolean", "number", "alert", "string"],
+        question: "Arrays in JavaScript can be used to contain ______.",
+        answers: ["numbers and strings", "numbers", "booleans", "all of the above"],
+        answerIndex: 3
+    },
+    {
+        question: "A useful tool for debugging is ______.",
+        answers: ["bug spray", "the terminal", "if/else", "the console"],
         answerIndex: 2
     },
     {
-        question: "The condition in an if/else statement is enclosed within ______.",
-        answers: ["quotes", "curly brackets", "parenthesis"],
-        answerIndex: 2
+        question: "Which is the correct method for saving to local storage?",
+        answers: [".save", ".setLocalStorage", ".getLocalStorage", ".rememberLocalValue"],
+        answerIndex: 1
+    },
+    {
+        question: "An array can be saved to local storage?",
+        answers: ["TRUE", "FALSE"],
+        answerIndex: 1
     }
 ]
